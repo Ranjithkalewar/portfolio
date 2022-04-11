766b14ad6a0aebf5b71c71d742273eb54756d374
kalewarranjith <Kalewars99>
2022-04-11 20:36:10 +0200

    Hyperlinks

---
+++ JavaScriptLearning/JavaBasics/JavaBasics.js
@@ -0,0 +1,4 @@
+document.write("hello im java script");
+document.getElementById("special") .innerHTML="Im form inner HTML";
+document.getElementById("normal") .innerHTML="yeah im normal";
+window.alert("Please click OK to continue");
