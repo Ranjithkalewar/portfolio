9667a5caccb14ffa114d9a3fbb5631a57b710369
kalewarranjith <Kalewars99>
2022-04-21 21:06:51 +0200

    java login

--- Practice/JavaScriptLearning/Learn JS/Scriptforlogin.js
+++ Practice/JavaScriptLearning/Learn JS/Scriptforlogin.js
@@ -2,18 +2,18 @@ let login = prompt("Who's there? " + " Answer : ((Admin))"); // Admin Question
 let usr = "Admin"; // Admin
 let pwd = "TheMaster"; // Password
 if (login === false || login == null) {
-// Empty feild || null is cancel button or Esc
+// Empty field || null is cancel button or Esc
     alert("Canceled"); // Message
 } else if (login !== usr) {
-// if Admin is have wrong value
+// if Admin have wrong value
     alert("I don't know you"); // Message
 } else {
 // if login == usr
     login = prompt("Password?" + "Answer : ((TheMaster))"); // show password question
     if (login == null || login === false) {
-// Empty feild || null is cancel button or Esc
+// Empty field || null is cancel button or Esc
         alert("Canceled"); // Message
     } else {
-        login === pwd ? alert("Welcome!") : alert("Wrong password"); // if Pasword is true : if Password is wrong
+        login === pwd ? alert("Welcome!") : alert("Wrong password"); // if Password is true : if Password is wrong
     }
 }
