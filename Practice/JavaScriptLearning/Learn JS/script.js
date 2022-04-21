8ff41eb2939919172ed0e1034290407bf7242a8d
kalewarranjith <Kalewars99>
2022-04-21 20:48:36 +0200

    java logics

--- Practice/JavaScriptLearning/Learn JS/script.js
+++ Practice/JavaScriptLearning/Learn JS/script.js
@@ -2,11 +2,31 @@
 /*let message = (login == 'Employee') ?'Hello' :
     (login == 'Director') ? 'Greetings':
         (login == '') ?'No login': '';
-alert(message)*/
+alert(message)
 let hour=11;
-if (hour<10 || hour>20){
+let min=30;
+/*if (hour<10 || hour>20){
     alert(`Office is Closed`);
 }
 else {
     alert(`welcome`);
+}
+if (hour==11 && min==30){
+    alert("Its 11:30")
+}*/
+let login=prompt(`Your Status`,'');
+if (login==='Admin') {
+    let pass = prompt(`password`, '');
+    if (pass === 'TheMaster') {
+        alert(`Welcome`);
+    } else if (pass === '' || pass === null) {
+        alert(`Cancelled`);
+    } else {
+        alert(`Wrong Password`);
+    }
+}
+else if (login===''||login===null){
+        alert(`Cancelled`)
+    } else {
+        alert(`I Dont Know You`)
 }
