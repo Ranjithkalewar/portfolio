7e5b3bc8d6106e1abf3fdd800b52915453a8bb78
kalewarranjith <Kalewars99>
2022-04-21 19:42:01 +0200

    java logics

---
+++ Practice/JavaScriptLearning/Learn JS/script.js
@@ -0,0 +1,12 @@
+
+/*let message = (login == 'Employee') ?'Hello' :
+    (login == 'Director') ? 'Greetings':
+        (login == '') ?'No login': '';
+alert(message)*/
+let hour=11;
+if (hour<10 || hour>20){
+    alert(`Office is Closed`);
+}
+else {
+    alert(`welcome`);
+}
