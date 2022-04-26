bb96690fd3364bb824f9ef14b29db64897fe66f8
kalewarranjith <Kalewars99>
2022-04-26 20:45:52 +0200

    date

---
+++ Practice/JavaScriptLearning/dateObjectives/date.js
@@ -0,0 +1,8 @@
+let d= new Date();
+document.write("date:"+d.getDate());
+
+document.write("day:"+d.getDay()+ '<br/>');
+document.write("Hours:"+d.getHours()+ '<br/>');
+document.write("Minuits:"+d.getMinutes()+ '<br/>');
+document.write("Time:"+d.getTime()+ '<br/>');
+document.write("Seconds:"+d.getSeconds()+ '<br/>');
